b71447990cf21f0b0db775d346228692dcc89e93
SimeonAtcitty <atcittysimo@gmail.com>
2016-11-04 11:29:48 -0600

    Added what i can today

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <?php require_once("lib/headUtils.php");?>
+  </head>
+  <body>
+
+    <div class="jumbotron">
+      <h1>My Awesome Site</h1>
+      <p>
+        Some info goes here.
+      </p>
+
+        <div class="row">
+          <div class="col-md-4">
+            <i class="fa fa-twitter"></i>
+          </div>
+          <div class="col-md-4">
+            <i class="fa fa-envelope"></i>
+          </div>
+          <div class="col-md-4">
+            <i class="fa fa-facebook"></i>
+          </div>
+
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+
+            <img class="imgThumbnail"src="http://tinyurl.com/jge78sv" style="height: 200px; width: 200px" alt="" />
+
+          </div>
+
+        </div>
+
+      </div>
+
+  </body>
+</html>
---
+++ lib/headUtils.php
@@ -0,0 +1,15 @@
+<!--  FONT AWESOME CDN LINK -->
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+<!-- THIS IS THE LINK FOR BOOTSTRAP -->
+<link rel="stylesheet" href="lib/css/bootstrap.css">
+
+<!--THIS IS THE LINK FOR MY OWN STYLESHEET-->
+<link rel="stylesheet" href="lib/css/style.css">
+
+
+<!--THIS IS THE CDN FOR JQUERY 2.2.4-->
+<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+
+<!--THIS IS FOR BOOTSTRAP.JS-->
+<script src="lib/js/bootstrap.js"></script>
